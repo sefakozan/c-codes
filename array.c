194454daf255744708ef7af7aba8b820705fc246
Sefa Kozan <sefakozan05@gmail.com>
2026-06-30 11:38:15 +0300

    Update array.c

--- array.c
+++ array.c
@@ -4,40 +4,37 @@ void dizi_tanim(void);
 void ters_dizi(void);
 void fib_array(void);
 
-int main()
-{
+int main() {
 
-    ters_dizi();
-    fib_array();
+  ters_dizi();
+  fib_array();
 
-    return 0;
+  return 0;
 }
 // 0 1 2 3 4
-void dizi_tanim()
-{
+void dizi_tanim() {
 
-    //--------------Tek Boyutlu Diziler//--------------
+  //--------------Tek Boyutlu Diziler//--------------
 
-    // int 4 btye(32 bit) yer kaplar, 10 elemanlık dizi 40 byte yer kaplar
-    int n[10];
+  // int 4 btye(32 bit) yer kaplar, 10 elemanlık dizi 40 byte yer kaplar
+  int n[10];
 
-    for (int i = 0; i < 10; i++)
-    {
-        n[i] = i;
-        printf("%d ", n[i]);
-    }
+  for (int i = 0; i < 10; i++) {
+    n[i] = i;
+    printf("%d ", n[i]);
+  }
 
-    int a[] = {2, 3, 9, 8, 15, -6}; // a[6] yazılabilir
-    // a[3] = 8, a[5] = -6
+  int a[] = {2, 3, 9, 8, 15, -6}; // a[6] yazılabilir
+  // a[3] = 8, a[5] = -6
 
-    //--------------Çok Boyutlu Diziler--------------
-    // Dikdörtgen yapı
+  //--------------Çok Boyutlu Diziler--------------
+  // Dikdörtgen yapı
 
-    // 2 Boyutlu
-    int b[2][3] = {{1, 2, 3}, {4, 5, 6}};
+  // 2 Boyutlu
+  int b[2][3] = {{1, 2, 3}, {4, 5, 6}};
 
-    // 2'den Çok Boyutlu ?????
-    int c[2][3][3][3]; // ([3][3][3]) küp, [2] -> 2 tane küp
+  // 2'den Çok Boyutlu ?????
+  int c[2][3][3][3]; // ([3][3][3]) küp, [2] -> 2 tane küp
 }
 
 // Verilen diziyi tersine çevirmek
@@ -48,56 +45,49 @@ void dizi_tanim()
     ters  :  3  6  2  7  8
 
 */
-void ters_dizi()
-{
+void ters_dizi() {
 
-    int x;
-    printf("5 elemanlik dizi giriniz\n");
-    int a[5];
-    for (int i = 0; i < 5; i++)
-    {
-        printf("%d. eleman: ", i);
-        scanf("%d", &x);
-        a[i] = x;
-    }
-    int swap;
-    for (int i = 0; i < 2; i++)
-    {
-        swap = a[i];
-        a[i] = a[4 - i];
-        a[4 - i] = swap;
-    }
-    for (int i = 0; i < 5; i++)
-    {
-        printf("a[%d] = %d\n", i, a[i]);
-    }
+  int x;
+  printf("5 elemanlik dizi giriniz\n");
+  int a[5];
+  for (int i = 0; i < 5; i++) {
+    printf("%d. eleman: ", i);
+    scanf("%d", &x);
+    a[i] = x;
+  }
+  int swap;
+  for (int i = 0; i < 2; i++) {
+    swap = a[i];
+    a[i] = a[4 - i];
+    a[4 - i] = swap;
+  }
+  for (int i = 0; i < 5; i++) {
+    printf("a[%d] = %d\n", i, a[i]);
+  }
 }
 
-void fib_array()
-{
+void fib_array() {
 
-    /*
+  /*
 
-        a b c
-        0 1 1 tanım
-        1 1 2
-        1 2 3
-        2 3 5
-        3 5 8
-    */
+      a b c
+      0 1 1 tanım
+      1 1 2
+      1 2 3
+      2 3 5
+      3 5 8
+  */
 
-    int fib[20];
-    fib[0] = 1;
-    fib[1] = 1;
-    fib[2] = 2;
+  int fib[20];
+  fib[0] = 1;
+  fib[1] = 1;
+  fib[2] = 2;
 
-    for (int i = 0; i < 18; i++)
-    {
-        fib[i + 2] = fib[i + 1] + fib[i];
-    }
+  for (int i = 0; i < 18; i++) {
+    fib[i + 2] = fib[i + 1] + fib[i];
+  }
 
-    for (int i = 0; i < 20; i++)
-    {
-        printf("%d.\t: %d\n", i, fib[i]);
-    }
+  for (int i = 0; i < 20; i++) {
+    printf("%d.\t: %d\n", i, fib[i]);
+  }
 }
